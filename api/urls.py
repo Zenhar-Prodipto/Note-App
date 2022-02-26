a53582fe64c4d4d366b213055ea63f135053d653
Zenhar-Prodipto <zenhar.mz@gmail.com>
2022-02-26 19:29:52 +0600

    feat: add CRUD functionalies to the note app

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.getRoutes, name="routes"),
     path("notes/", views.getNotes, name="notes"),
+    path("notes/create", views.createNote, name="create-note"),
     path("notes/<str:id>/", views.getNote, name="note"),
+    path("notes/update/<str:id>", views.updateNote, name="update-note"),
+    path("notes/delete/<str:id>", views.deleteNote, name="delete-note"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from email import message
 from urllib import response
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -5,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.models import Note
 from .serializer import NoteSerializer
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
 
 # Create your views here.
 
@@ -56,8 +59,40 @@ def getNotes(req):
     return Response(serializer.data)
 
 
+@api_view(["POST"])
+def createNote(req):
+    data = req.data
+    serializer = NoteSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(["GET"])
 def getNote(req, id):
     note = Note.objects.get(id=id)
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def updateNote(req, id):
+    data = req.data
+    note = Note.objects.get(id=id)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def deleteNote(req, id):
+    note = get_object_or_404(Note, id=id)
+    serializer = NoteSerializer(instance=note, many=False)
+    title = serializer.data.get("title")
+    note.delete()
+
+    return Response({"msg": "success", "title": title}, status=status.HTTP_200_OK)
